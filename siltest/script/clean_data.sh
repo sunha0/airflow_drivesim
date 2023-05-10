779f2d28c7be5fee7c2fa2b896b498473270392d
sunhao <hao.sun@t-systems.com>
2023-05-10 12:38:16 +0800

    add comment

--- siltest/script/clean_data.sh
+++ siltest/script/clean_data.sh
@@ -2,3 +2,6 @@
 siltestDir="$1"
 rm -rf  ${siltestDir}/cache/dockerovcache-dev/.nvidia-omniverse/logs/Kit/omni.drivesim.e2e/23.1/*
 rm -rf  ${siltestDir}/rrLog/*
+rm -rf  ${siltestDir}/silEvaluationInput/*
+
+echo "=======================cache & rrLog directory has deleted.======================="
--- siltest/script/stop_ndas_drsivesim_container.sh
+++ siltest/script/stop_ndas_drsivesim_container.sh
@@ -2,9 +2,11 @@
 
 if [ ! -z $(docker ps -q -f "name=ndas")  ];then
 docker kill ndas
+echo  "=======================ndas has killed======================="
 fi
 
 if [ ! -z $(docker ps -q -f "name=drivesim-ov") ];then
 docker kill drivesim-ov
+echo  "=======================drivesim-ov has killed======================="
 fi
 
